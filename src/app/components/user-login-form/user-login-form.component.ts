e990382c5953d8647fe9b513babacb32feca8f78
lucasmartins2478 <129237076+lucasmartins2478@users.noreply.github.com>
2024-09-19 19:36:49 -0300

    corrigindo responsividade dos articles da página de home

--- src/app/components/user-login-form/user-login-form.component.ts
+++ src/app/components/user-login-form/user-login-form.component.ts
@@ -6,9 +6,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrl: './user-login-form.component.css'
+  styleUrl: './user-login-form.component.css',
 })
-export class UserLoginFormComponent implements OnInit{
+export class UserLoginFormComponent implements OnInit {
   loginForm!: FormGroup;
 
   constructor(
@@ -18,41 +18,47 @@ export class UserLoginFormComponent implements OnInit{
   ) {}
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      user: ['', [Validators.required, Validators.email]],  // Correção na sintaxe do array de validadores
-      password: ['', [Validators.required, Validators.minLength(6)]],  // Correção aqui também
+      email: ['', [Validators.required, Validators.email]], // Correção na sintaxe do array de validadores
+      password: ['', [Validators.required, Validators.minLength(6)]], // Correção aqui também
     });
   }
 
   onSubmit() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
 
+      const apiUrl = 'https://664d0a0cede9a2b556527d60.mockapi.io/api/v1/users';
 
-    if ( this.loginForm.valid){
-      window.alert("Dados corretos")
-    }else{
-      window.alert("Dados incorretos")
+      // Fazendo a requisição GET
+      this.http.get<any[]>(apiUrl).subscribe(
+        (response) => {
+          response.forEach((user) => {
+
+            if(user.email == formData.email){
+              if(user.password == formData.password){
+                window.alert('Usuário e senha corretos!');
+                this.router.navigate(['/vagas']);
+              }
+              else{
+                window.alert("Senha não confere!")
+              }
+            }
+            // if (
+            //   user.email == formData.user &&
+            //   user.password == formData.password
+            // ) {
+            //
+            //
+            // }
+          });
+
+        },
+        (error) => {
+          window.alert(`Erro ao buscar dados, ${error}`);
+        }
+      );
+    } else {
+      window.alert('Preencha os campos corretamente!');
     }
-    // const formData = this.loginForm.value;
-    // let isLogged: boolean = false;
-
-    // const apiUrl = 'https://664d0a0cede9a2b556527d60.mockapi.io/api/v1/users';
-
-    // // Fazendo a requisição GET
-    // this.http.get<any[]>(apiUrl).subscribe(
-    //   (response) => {
-
-
-    //     response.forEach(user=>{
-    //       if(user.email == formData.user && user.password == formData.password){
-    //         window.alert("usuário e senha corretos")
-    //         this.router.navigate(['/vagas']);
-    //       }
-    //     })
-    //   },
-    //   (error) => {
-    //     window.alert(`Erro ao buscar dados, ${error}`);
-    //   }
-    // );
-    // window.alert(`dados do formulário : ${formData.user}, ${formData.password}`);
-    // this.router.navigate(['/vagas']);
   }
 }
